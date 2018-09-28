9a502c03504ad3f8914ea423e307c721c86b46c6
Shruti Shankar <42975636+sshankar21@users.noreply.github.com>
2018-09-28 12:27:57 -0400

    Add files via upload

---
+++ src/OperationsTest.java
@@ -0,0 +1,22 @@
+import org.graalvm.compiler.lir.StandardOp.SaveRegistersOp;
+
+public class OperationsTest 
+{
+	
+	public static void main(String[]args)
+	{
+		int number; //declaration statement
+		number = 20; //Assignment statement -> initialization
+		number += 5;
+		System.out.println(number);
+		number /= 2;
+		System.out.println(number);
+		number -= 6;
+		System.out.println(number);
+		number *= 4;
+		System.out.println(number);
+		number%=2;
+		System.out.println(number);
+	}
+	
+}
